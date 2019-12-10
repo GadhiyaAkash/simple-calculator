7233b1793836392b7bf7323fe83f42865b5ae4a2
Akash Patel <akash.patel@vortilla.com>
2019-12-10 15:56:34 +0530

    Added simple logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,95 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Simple Calculator App';
+  calInput: string = '';
+  @ViewChild('search', { static: true }) searchElement: ElementRef;
+
+  constructor() {
+  }
+
+  preCalculatValue() {
+
+  }
+
+  clickZero() {
+    this.calInput += '0';
+  }
+
+  clickOne() {
+    this.calInput += '1';
+  }
+
+  clickTwo() {
+    this.calInput += '2';
+  }
+
+  clickThree() {
+    this.calInput += '3';
+  }
+
+  clickFour() {
+    this.calInput += '4';
+  }
+
+  clickFive() {
+    this.calInput += '5';
+  }
+
+  clickSix() {
+    this.calInput += '6';
+  }
+
+  clickSeven() {
+    this.calInput += '7';
+  }
+
+  clickEight() {
+    this.calInput += '8';
+  }
+
+  clickNine() {
+    this.calInput += '9';
+  }
+
+  clickAC() {
+    this.focusInput();
+    this.calInput = '';
+  }
+
+  clickCross() {
+    this.calInput = '9';
+  }
+
+  clickDivision() {
+    this.calInput += '/';
+  }
+
+  clickMultiplication() {
+    this.calInput += '*';
+  }
+
+  clickMinus() {
+    this.calInput += '-';
+  }
+
+  clickPlus() {
+    this.calInput += '+';
+  }
+
+  clickDot() {
+    this.calInput += '.';
+  }
+
+  clickEqual() {
+    this.calInput += '=';
+  }
+
+  /**
+   * Helper function to focus on input
+   */
+  focusInput() {
+    setTimeout(() => {
+      this.searchElement.nativeElement.focus();
+    }, 0);
+  }
 }
